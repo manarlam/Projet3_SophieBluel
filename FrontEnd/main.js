3d74e5ef2db54ebb54491223277978ebdb23ada0
Manar <75319989+canardeguerre@users.noreply.github.com>
2025-03-11 21:45:37 +0100

    Filtres_WIP

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -16,6 +16,45 @@ async function getWorks() {
 
 getWorks();
 
+async function getFilters() {
+    const url = "http://localhost:5678/api/categories";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+        genererFiltre(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+getFilters();
+
+// Intégration des filtres dans la page
+
+const container = document.createElement("div");
+document.body.appendChild(container);
+
+function genererFiltre(categories) {
+    for (let i = 0; i < categories.length; i++) {
+
+        const btn = document.createElement("button");
+        btn.innerHTML = `<button>${categories[i].name}</button>`;
+        
+        container.appendChild(btn);
+
+
+    }
+}
+
+
+
+// Intégration des oeuvres dans la page
+
 function genererOeuvre(oeuvre) {
     for (let i = 0; i < oeuvre.length; i++) {
 
